dfd6be334dc48dba24856b9446adad0112417417
OzerOzturk <ozerozturk1636@gmail.com>
2022-08-06 14:23:32 +0300

    feat(Filter.js) add filter component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { CartState } from "../context/Context";
+import Filters from "./Filters";
 import SingleProduct from "./SingleProduct";
 import "./styles.css";
 
@@ -11,7 +12,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {/* {<Filters/>} */}
+       <Filters/>
       <div className="productContainer">
         {products.map((prod) => {
           return <SingleProduct prod={prod} key={prod.id} />;
